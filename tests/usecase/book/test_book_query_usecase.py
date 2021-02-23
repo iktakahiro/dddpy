f4e63d6934359f4d374abd25264d4369aee78b6b
iktakahiro <takahiro.ikeuchi@gmail.com>
2021-02-23 06:25:05 +0000

    Add test_fetch_books_should_return_books

--- tests/usecase/book/test_book_query_usecase.py
+++ tests/usecase/book/test_book_query_usecase.py
@@ -42,3 +42,25 @@ class TestBookQueryUseCase:
         with pytest.raises(BookNotFoundError):
             book_query_usecase.fetch_book_by_id("cPqw4yPVUM3fA9sqzpZmkL")
 
+    def test_fetch_books_should_return_books(self):
+        session = MagicMock()
+        book_query_service = BookQueryServiceImpl(session)
+        book_query_service.find_all = Mock(
+                return_value=[BookQueryModel(
+                    id="cPqw4yPVUM3fA9sqzpZmkL",
+                    isbn="978-0141983479",
+                    title="Bullshit Job",
+                    page=320,
+                    read_page=126,
+                    created_at=1614051983128,
+                    updated_at=1614056689464,
+                )]
+            )
+
+        book_query_usecase = BookQueryUseCaseImpl(book_query_service)
+
+        books = book_query_usecase.fetch_books()
+
+        assert len(books) == 1
+
+        assert books[0].page == 320
