59f2a1d7131e02f9c564ef5a36cd67ecbfde0932
iktakahiro <takahiro.ikeuchi@gmail.com>
2021-02-23 06:22:43 +0000

    Implement TestBookQueryUseCase

---
+++ tests/usecase/book/test_book_query_usecase.py
@@ -0,0 +1,44 @@
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
+
+from dddpy.infrastructure.sqlite.book.book_query_service import BookQueryServiceImpl
+from dddpy.usecase.book.book_query_model import BookQueryModel
+from dddpy.usecase.book.book_query_service import BookQueryService
+from dddpy.usecase.book.book_query_usecase import BookQueryUseCase, BookQueryUseCaseImpl
+from dddpy.domain.book.book_exception import BookNotFoundError
+
+class TestBookQueryUseCase:
+    def test_fetch_book_by_id_should_return_book(self):
+
+        session = MagicMock()
+        book_query_service = BookQueryServiceImpl(session)
+        book_query_service.find_by_id = Mock(
+                return_value=BookQueryModel(
+                    id="cPqw4yPVUM3fA9sqzpZmkL",
+                    isbn="978-0141983479",
+                    title="Bullshit Job",
+                    page=320,
+                    read_page=126,
+                    created_at=1614051983128,
+                    updated_at=1614056689464,
+                )
+            )
+
+        book_query_usecase = BookQueryUseCaseImpl(book_query_service)
+
+        book = book_query_usecase.fetch_book_by_id("cPqw4yPVUM3fA9sqzpZmkL")
+
+        assert book.title == "Bullshit Job"
+
+    def test_fetch_book_by_id_should_throw_book_not_found_error(self):
+
+        session = MagicMock()
+        book_query_service = BookQueryServiceImpl(session)
+        book_query_service.find_by_id = Mock(side_effect=BookNotFoundError)
+
+        book_query_usecase = BookQueryUseCaseImpl(book_query_service)
+
+        with pytest.raises(BookNotFoundError):
+            book_query_usecase.fetch_book_by_id("cPqw4yPVUM3fA9sqzpZmkL")
+
