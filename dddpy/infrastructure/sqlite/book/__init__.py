d2e8885c9d608626f2b238e2de164fba6fcb07f9
Takahiro Ikeuchi <takahiro.ikeuchi@awarefy.com>
2024-07-26 15:20:57 +0000

    chore: add Ruff

--- dddpy/infrastructure/sqlite/book/__init__.py
+++ dddpy/infrastructure/sqlite/book/__init__.py
@@ -1,3 +1,6 @@
 from .book_dto import BookDTO
 from .book_query_service import BookQueryServiceImpl
-from .book_repository import BookCommandUseCaseUnitOfWorkImpl, BookRepositoryImpl
+from .book_repository import (
+    BookCommandUseCaseUnitOfWorkImpl,
+    BookRepositoryImpl,
+)
